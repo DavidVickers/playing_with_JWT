b4280383b9d554c998a7879fe2321f460d07b7a2
DavidVickers <david.vickers@icloud.com>
2021-10-19 09:30:39 -0400

    code mod

--- JWTToken.py
+++ JWTToken.py
@@ -39,10 +39,7 @@ print("Instance URL", instance_url)
 
 
 def sf_api_call(action, parameters = {}, method = 'get', data = {}):
-    """
-    Helper function to make calls to Salesforce REST API.
-    Parameters: action (the URL), URL params, method (get, post or patch), data for POST/PATCH.
-    """
+    
     headers = {
         'Content-type': 'application/json',
         'Accept-Encoding': 'gzip',
@@ -53,7 +50,7 @@ def sf_api_call(action, parameters = {}, method = 'get', data = {}):
     elif method in ['post', 'patch']:
         r = requests.request(method, instance_url+action, headers=headers, json=data, params=parameters, timeout=10)
     else:
-        # other methods not implemented in this example
+        # error for methods not implemented in this code
         raise ValueError('Method should be get or post or patch.')
     print('Debug: API %s call: %s' % (method, r.url) )
     if r.status_code < 300:
